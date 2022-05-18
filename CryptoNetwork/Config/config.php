15ac7afd12c935fd85a004b305ccde05bd79f33d
mohsen bagheri <bagheriimohsen@gmail.com>
2022-05-18 16:29:11 +0430

    add network service into config file

--- CryptoNetwork/Config/config.php
+++ CryptoNetwork/Config/config.php
@@ -1,5 +1,33 @@
 <?php
 
+use Modules\CryptoNetwork\Services\BinanceService;
+use Modules\CryptoNetwork\Services\BinanceSmartChainService;
+use Modules\CryptoNetwork\Services\BitcoinService;
+use Modules\CryptoNetwork\Services\ByteTradeService;
+use Modules\CryptoNetwork\Services\CardanoService;
+use Modules\CryptoNetwork\Services\DogeService;
+use Modules\CryptoNetwork\Services\EtcService;
+use Modules\CryptoNetwork\Services\EthService;
+use Modules\CryptoNetwork\Services\LiteCoinService;
+use Modules\CryptoNetwork\Services\PolkadotService;
+use Modules\CryptoNetwork\Services\TestnetService;
+use Modules\CryptoNetwork\Services\TronService;
+
 return [
-    'name' => 'CryptoNetwork'
+    'name' => 'CryptoNetwork',
+
+    'networks' => [
+        BitcoinService::symbol() => BitcoinService::class,
+        BinanceService::symbol() => BinanceService::class,
+        BinanceSmartChainService::symbol() => BinanceSmartChainService::class,
+        ByteTradeService::symbol() => ByteTradeService::class,
+        CardanoService::symbol() => CardanoService::class,
+        DogeService::symbol() => DogeService::class,
+        EtcService::symbol() => EtcService::class,
+        EthService::symbol() => EthService::class,
+        LiteCoinService::symbol() => LiteCoinService::class,
+        PolkadotService::symbol() => PolkadotService::class,
+        TestnetService::symbol() => TestnetService::class,
+        TronService::symbol() => TronService::class,
+    ]
 ];
